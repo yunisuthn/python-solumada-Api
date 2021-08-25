59214b3127304c54896895c22fa6bd8e7571cf67
yunisuthn <yunis.thn@gmail.com>
2021-08-25 08:49:23 +0300

    configuration

--- app_pdf/settings.py
+++ app_pdf/settings.py
@@ -22,13 +22,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yyima#=x40vp!aat7u-5ez&&j_l(*n+-kv%ix9mk0o5m1=(z0t'
+# SECRET_KEY = 'django-insecure-yyima#=x40vp!aat7u-5ez&&j_l(*n+-kv%ix9mk0o5m1=(z0t'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
 
+ALLOWED_HOSTS = ['name-upload.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
@@ -159,5 +160,3 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
-
-ALLOWED_HOSTS = ['name-upload.herokuapp.com']
